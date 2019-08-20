f92dd6d3d8aee81b8fff112c070dcf9df9ba000b
rishivedpathak <rsvedpathak@gmail.com>
2019-08-20 13:02:54 +0530

    Updates

---
+++ precache-manifest.0b9ff8339b1a0a61d606c3fbbbd0b6f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c95ae864c39d7f72ee6c",
+    "url": "/react-gotjs-example/static/js/runtime~main.31357bc2.js"
+  },
+  {
+    "revision": "6cc5c5b431ded23765a5",
+    "url": "/react-gotjs-example/static/js/main.a2af880a.chunk.js"
+  },
+  {
+    "revision": "523ff37afdd3b58874ba",
+    "url": "/react-gotjs-example/static/js/2.29e92fb7.chunk.js"
+  },
+  {
+    "revision": "6cc5c5b431ded23765a5",
+    "url": "/react-gotjs-example/static/css/main.4928852b.chunk.css"
+  },
+  {
+    "revision": "34baad77181951c31d108b304931ad77",
+    "url": "/react-gotjs-example/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gojs-example/precache-manifest.7ae7cb84f2d3a78a1e17f3f94d76da4c.js"
+  "/react-gotjs-example/precache-manifest.0b9ff8339b1a0a61d606c3fbbbd0b6f0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gojs-example/index.html", {
+workbox.routing.registerNavigationRoute("/react-gotjs-example/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
